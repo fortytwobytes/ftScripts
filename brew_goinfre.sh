d408877eeeb557994eb83a27370b0d0951afcc24
aymane330 <aymanefilali330@gmail.com>
2023-05-30 08:37:22 +0100

    script that install brew in goinfre

---
+++ brew_goinfre.sh
@@ -0,0 +1,52 @@
+rm -rf $HOME/goinfre/.brew
+git clone --depth=1 https://github.com/Homebrew/brew $HOME/goinfre/.brew
+
+cat > $HOME/.brewconfig.zsh <<EOL
+# HOMEBREW CONFIG
+
+# Add brew to path
+export PATH=\$HOME/goinfre/.brew/bin:\$PATH
+
+# Set Homebrew temporary folders
+export HOMEBREW_CACHE=/tmp/\$USER/Homebrew/Caches
+export HOMEBREW_TEMP=/tmp/\$USER/Homebrew/Temp
+
+mkdir -p \$HOMEBREW_CACHE
+mkdir -p \$HOMEBREW_TEMP
+
+# If NFS session
+# Symlink Locks folder in /tmp
+if df -T autofs,nfs \$HOME 1>/dev/null
+then
+  HOMEBREW_LOCKS_TARGET=/tmp/\$USER/Homebrew/Locks
+  HOMEBREW_LOCKS_FOLDER=\$HOME/goinfre/.brew/var/homebrew
+
+  mkdir -p \$HOMEBREW_LOCKS_TARGET
+  mkdir -p \$HOMEBREW_LOCKS_FOLDER
+
+  # Symlink to Locks target folders
+  # If not already a symlink
+  if ! [[ -L \$HOMEBREW_LOCKS_FOLDER && -d \$HOMEBREW_LOCKS_FOLDER ]]
+  then
+     echo "Creating symlink for Locks folder"
+     rm -rf \$HOMEBREW_LOCKS_FOLDER
+     ln -s \$HOMEBREW_LOCKS_TARGET \$HOMEBREW_LOCKS_FOLDER
+  fi
+fi
+EOL
+
+if ! grep -q "# Load Homebrew config script" $HOME/.zshrc
+then
+cat >> $HOME/.zshrc <<EOL
+
+# Load Homebrew config script
+source \$HOME/.brewconfig.zsh
+EOL
+fi
+
+source $HOME/.brewconfig.zsh
+rehash
+brew update
+
+
+echo "\nPlease open a new shell to finish installation"
